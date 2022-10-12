ee52a2147aa3ca2c98e35fdf09027a6a54dd9f0e
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-10-12 11:24:23 +0100

    we

--- interfaces/IFoodManager.cs
+++ interfaces/IFoodManager.cs
@@ -5,7 +5,7 @@ namespace FDM.interfaces
     {
          public bool ICreateFood(string foodName, double price);
          public void IUpdateFood(string foodName, double price);
-         public void IDeleteFood(Food food);
+         public void IDeleteFood(Food food); //deleting whole instance of food object
          public void IGetFood(string foodName);//get before deleting and delete
          public void IGetFood(int refNumber);
 
