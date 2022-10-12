a51a9e76a1646870bc97ccdf73a71ada422cefa2
Treehays90 <33053528+Treehays90@users.noreply.github.com>
2022-10-12 10:55:52 +0100

    help

--- models/Food.cs
+++ models/Food.cs
@@ -3,11 +3,11 @@ namespace FDM.models
     public class Food
     {
         public string FoodName { get; set; }
-        public double price { get; set; }
+        public double Price { get; set; }
         public Food(string foodName, double price)
         {
             FoodName = foodName;
-            price = price;
+            Price = price;
         }
         // ref Number(int)
         
